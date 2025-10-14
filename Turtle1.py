9818d69220580b4e717c380a291a04fdef5bc942
Umer-1-14-debug <m.umer0000013@gmail.com>
2025-10-14 14:22:03 +0500

    Add files via upload

---
+++ Turtle1.py
@@ -0,0 +1,16 @@
+import turtle as t
+t.Screen().bgcolor("blue")
+t.Screen().setup(500, 500)
+p=t.Turtle()
+sides=3
+angle=180
+shape=angle/sides
+p.forward(120)
+p.left(100)
+p.forward(120)
+p.left(100)
+p.forward(150)
+p.right(50)
+p.penup()
+t.done()
+
---
+++ square.py
@@ -0,0 +1,7 @@
+import turtle as t
+p = t.Turtle()
+t.speed(5)
+for i in range(4):
+    p.forward(100)
+    p.right(90)
+p.done()
